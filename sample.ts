46a18ff5e4411d3363e261acf9be8b3ab6f66678
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2019-12-27 10:45:45 +0900

    Add files via upload

---
+++ sample.ts
@@ -0,0 +1,18 @@
+import RPA from "ts-rpa";
+
+(async () => {
+  try {
+    await RPA.WebBrowser.get("https://www.google.com/");
+    const input = await RPA.WebBrowser.findElement(".gLFyf");
+    await RPA.WebBrowser.sendKeys(input, ["RPA"]);
+    const element = await RPA.WebBrowser.findElement(
+      ".FPdoLc > center:nth-child(1) > input:nth-child(2)"
+    );
+    await RPA.WebBrowser.mouseClick(element);
+    await RPA.sleep(3000);
+  } catch (error) {
+    RPA.SystemLogger.error(error);
+  } finally {
+    await RPA.WebBrowser.quit();
+  }
+})();
