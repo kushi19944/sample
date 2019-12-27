9e3cf1767111bea8e5f1b460ca2436a719865d74
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2019-12-27 17:26:11 +0900

    Update sample_1.ts

--- sample_1.ts
+++ sample_1.ts
@@ -3,9 +3,7 @@ import RPA from "ts-rpa";
 (async () => {
   try {
     await RPA.WebBrowser.get("https://www.google.com/");
-    const input = await RPA.WebBrowser.wait(RPA.WebBrowser.Until.elementLocated({xpath:'//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input'}),5000);
-    await RPA.WebBrowser.sendKeys(input,[`${google}`]);
-    const ele = await RPA.WebBrowser.findElementByXPath('//*[@id="hptl"]/a[2]');
+    const ele = await RPA.WebBrowser.wait(RPA.WebBrowser.Until.elementLocated({xpath:'//*[@id="hptl"]/a[2]'}),5000);
     const text = await ele.getText();
     if(text == "ストア"){
      await RPA.WebBrowser.mouseClick(ele); 
