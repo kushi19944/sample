40e89140a6b3cbbbfdbe89458c36acc54cca35ed
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2019-12-27 13:25:59 +0900

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -4,8 +4,8 @@ import RPA from "ts-rpa";
   try {
     await RPA.WebBrowser.get("https://github.com/kushi19944/sample/");
     for(let i = 0; i < 30; i++){
-     RPA.Logger.log(i);
-     RPA.sleep(500);
+     await RPA.Logger.log(i);
+     await RPA.sleep(500);
     }
     await RPA.WebBrowser.takeScreenshot();
   } catch (error) {
