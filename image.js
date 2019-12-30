5381630451cf3e7b02480effac72123b9dd79db0
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2019-12-31 03:50:27 +0900

    Update image.js

--- image.js
+++ image.js
@@ -9,29 +9,21 @@ var name2_1 = 'sample/2_1.jpg'
 async function test(){
   imghash
   .hash(name1)
-  .then((hash) => {
-    await console.log(name1 + ' '+ hash); // 'f884c4d8d1193c07'
-  });
+    await console.log(name1 + ' '+ hash);
 
 imghash
   .hash(name1_1)
-  .then((hash) => {
     await console.log(name1_1 + ' '+ hash); // 'f884c4d8d1193c07'
-  });
 
 
 imghash
   .hash(name2)
-  .then((hash) => {
-    await console.log(name2 + ' '+ hash); // 'f884c4d8d1193c07'
-  });
+    await console.log(name2 + ' '+ hash);
  
 // Custom hex length and result in binary
 imghash
   .hash(name2_1, 4, 'binary')
-  .then((hash) => {
     await console.log(name2_1+' '+hash); // '1000100010000010'
-  });
 
 
 await Promise
